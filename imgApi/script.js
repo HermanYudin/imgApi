e1149953b9890d3d7baa707311c056d5bb468477
HermanYudin <hermanhockey1994@yandex.ru>
2023-09-28 22:56:48 +0300

    feat:some problems with js were complete

--- imgApi/script.js
+++ imgApi/script.js
@@ -1,14 +1,15 @@
-const accessKey = '5Fhq68dS3K3CbXarxRqmBEgK2BdE81Z5-F2LCjpeFag';
+const accessKey = '8VBpwDgQ7FskYny1DWRt4qU9Uj8DobOB0AHDkH0kFFE';
 const form = document.querySelector('.img-form');
 const input = document.querySelector('.search-input');
 const searchRes = document.querySelector('.search-results');
 const showMoreBtn = document.querySelector('.show-more-btn');
 let inputData = '';
 let page = 1;
-const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
 async function searchImages(){
     inputData = input.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
     const response = await fetch(url);
     const data = await response.json();
     const results = data.results;
@@ -21,23 +22,28 @@ async function searchImages(){
         const imageWrapper = document.createElement('div');
         imageWrapper.classList.add('search-result');
         const image = document.createElement('img');
+        image.classList.add('search-result-img');
         image.src = result.urls.small;
         image.alt = result.alt_description;
+        const imageLinkContainer = document.createElement('div');
+        imageLinkContainer.classList.add('search-result-link-container');
         const imageLink = document.createElement('a');
+        imageLink.classList.add('search-result-link');
         imageLink.href = result.links.html;
         imageLink.target = '_blank';
         imageLink.textContent = result.alt_description;
 
         imageWrapper.appendChild(image);
-        imageWrapper.appendChild(imageLink);
-        imageWrapper.appendChild(imageWrapper);
+        imageWrapper.appendChild(imageLinkContainer);
+        imageLinkContainer.appendChild(imageLink);
+        searchRes.appendChild(imageWrapper);
+    });
 
-        page++;
-        
-        if(page > 1){
+    page++;
+
+    if(page > 1){
             showMoreBtn.style.display = 'block';
-        }
-    })
+    }
 };
 
 form.addEventListener('submit',(event) => {
@@ -48,4 +54,4 @@ searchImages();
 
 showMoreBtn.addEventListener('click',() => {
     searchImages();
-    });
+});

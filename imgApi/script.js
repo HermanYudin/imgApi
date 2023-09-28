dd57cc626f45af7ba464133eefe1ea5820aac476
HermanYudin <hermanhockey1994@yandex.ru>
2023-09-28 21:25:36 +0300

    feat:js file were edited

--- imgApi/script.js
+++ imgApi/script.js
@@ -16,4 +16,36 @@ async function searchImages(){
     if(page === 1){
         searchRes.innerHTML = '';
     }
-}
+
+    results.map((result) => {
+        const imageWrapper = document.createElement('div');
+        imageWrapper.classList.add('search-result');
+        const image = document.createElement('img');
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement('a');
+        imageLink.href = result.links.html;
+        imageLink.target = '_blank';
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        imageWrapper.appendChild(imageWrapper);
+
+        page++;
+        
+        if(page > 1){
+            showMoreBtn.style.display = 'block';
+        }
+    })
+};
+
+form.addEventListener('submit',(event) => {
+event.preventDefault();
+page = 1;
+searchImages();
+});
+
+showMoreBtn.addEventListener('click',() => {
+    searchImages();
+    });
